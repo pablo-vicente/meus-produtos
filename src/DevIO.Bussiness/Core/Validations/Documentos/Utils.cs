6ff54a91d02ec238002db7a78fc956879c20a96f
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-05-24 20:42:25 -0300

    Validator

---
+++ src/DevIO.Bussiness/Core/Validations/Documentos/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace DevIO.Business.Core.Validations.Documentos
+{
+    public class Utils
+    {
+        public static string ApenasNumeros(string valor)
+        {
+            var onlyNumber = valor.Where(s => char.IsDigit(s)).Aggregate("", (current, s) => current + s);
+            return onlyNumber.Trim();
+        }
+    }
+}
--- src/DevIO.Bussiness/Models/Fornecedores/Validations/FornecedorValidation.cs
+++ src/DevIO.Bussiness/Models/Fornecedores/Validations/FornecedorValidation.cs
@@ -1,3 +1,4 @@
+using DevIO.Business.Core.Validations.Documentos;
 using FluentValidation;
 
 namespace DevIO.Bussiness.Models.Fornecedores.Validations
@@ -11,6 +12,27 @@ namespace DevIO.Bussiness.Models.Fornecedores.Validations
                 .WithMessage("O campo {propertyName} precisa ser fornecido")
                 .Length(2, 200)
                 .WithMessage("O campo {propertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            When(x => x.TipoFornecedor is TipoFornecedor.PessoaFisica, () =>
+            {
+                RuleFor(x => x.Documento.Length)
+                    .Equal(CpfValidacao.TamanhoCpf)
+                    .WithMessage("O campo Documento precisa ter {ComparisonValue} caracters e foi fornecido {PropertyValye}");
+
+                RuleFor(x => CpfValidacao.Validar(x.Documento)).Equal(true)
+                    .WithMessage("o documento fornecido é inválido");
+            });
+            
+            When(x => x.TipoFornecedor is TipoFornecedor.PessoaJurifica, () =>
+            {
+                
+                RuleFor(x => x.Documento.Length)
+                    .Equal(CnpjValidacao.TamanhoCnpj)
+                    .WithMessage("O campo Documento precisa ter {ComparisonValue} caracters e foi fornecido {PropertyValye}");
+
+                RuleFor(x => CnpjValidacao.Validar(x.Documento)).Equal(true)
+                    .WithMessage("o documento fornecido é inválido");
+            });
         }
             
     }
