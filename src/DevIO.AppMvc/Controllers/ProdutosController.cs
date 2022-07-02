f133dca750a4e273f7347baef330bbc7fd9a0bb0
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-07-02 11:23:33 -0300

    Produtos Controller

--- src/DevIO.AppMvc/Controllers/ProdutosController.cs
+++ src/DevIO.AppMvc/Controllers/ProdutosController.cs
@@ -4,11 +4,8 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using AutoMapper;
 using DevIO.AppMvc.ViewModels;
-using DevIO.Bussiness.Models.Fornecedores.Services;
 using DevIO.Bussiness.Models.Produtos;
 using DevIO.Bussiness.Models.Produtos.Services;
-using DevIO.Bussiness.Notificacoes;
-using DevIO.Infra.Data.Repositories;
 
 namespace DevIO.AppMvc.Controllers
 {
@@ -22,6 +19,8 @@ namespace DevIO.AppMvc.Controllers
         {
           
         }
+        [Route("lista-de-produtos")]
+        [HttpGet]
         public async Task<ActionResult> Index()
         {
             var produtos = await _produtoRepository.ObterTodosAsync();
@@ -30,21 +29,89 @@ namespace DevIO.AppMvc.Controllers
             return View(produtosViewModels);
         }
 
+        [Route("dados-do-produto/{id:guid}")]
+        [HttpGet]
         public async Task<ActionResult> Details(Guid id)
         {
             var produto = await _produtoRepository.ObterPorIdAsync(id);
+
+            if (produto is null)
+                return HttpNotFound();
             var produtoViewModel = _mapper.Map<ProdutoViewModel>(produto);
             
             return View(produtoViewModel);
         }
 
-        // public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
-        // {
-        //     if (ModelState.IsValid)
-        //     {
-        //         var produto = _mapper.Map<Produto>(produtoViewModel);
-        //         await _produtoService.AdicionarAsync(produto);
-        //     }
-        // }
+        [Route("novo-produto")]
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+        
+        [Route("novo-produto")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) 
+                return View(produtoViewModel);
+            
+            var produto = _mapper.Map<Produto>(produtoViewModel);
+            await _produtoService.AdicionarAsync(produto);
+
+            return RedirectToAction("Index");
+        }
+
+        [Route("editar-produto/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> Edit(Guid id)
+        {
+            var produto = await _produtoRepository.ObterPorIdAsync(id);
+
+            if (produto is null)
+                return HttpNotFound();
+            var produtoViewModel = _mapper.Map<ProdutoViewModel>(produto);
+
+           return View(produtoViewModel);
+        }
+        
+        [Route("editar-produto/{id:guid}")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(produtoViewModel);
+
+            var produto = _mapper.Map<Produto>(produtoViewModel);
+            await _produtoService.AtualizarAsync(produto);
+
+            return RedirectToAction("Index");
+        }
+        
+        [Route("excluir-produto/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var produto = await _produtoRepository.ObterPorIdAsync(id);
+            var produtoViewModel = _mapper.Map<ProdutoViewModel>(produto);
+
+            return View(produtoViewModel);
+        }
+
+        [Route("editar-produto/{id:guid}")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfimerd(Guid id)
+        {
+            var produto = await _produtoRepository.ObterPorIdAsync(id);
+
+            if (produto is null)
+                return HttpNotFound();
+            
+            await _produtoService.RemoverAsync(id);
+            return RedirectToAction("Index");
+        }
     }
 }
