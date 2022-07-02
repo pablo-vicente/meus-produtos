a77a2276ab808cd246489eaede0d06c7ec26aebb
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-07-02 15:54:48 -0300

    Java Script

--- src/DevIO.AppMvc/App_Start/BundleConfig.cs
+++ src/DevIO.AppMvc/App_Start/BundleConfig.cs
@@ -20,6 +20,9 @@ namespace DevIO.AppMvc
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/siteJs").Include(
+                "~/Scripts/site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
