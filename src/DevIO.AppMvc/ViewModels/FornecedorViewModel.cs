22492804e9dda0dc00d90c85c575ee96718f3143
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-07-02 14:46:42 -0300

    Fix

--- src/DevIO.AppMvc/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.AppMvc/ViewModels/FornecedorViewModel.cs
@@ -20,9 +20,9 @@ namespace DevIO.AppMvc.ViewModels
         [StringLength(100, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracters", MinimumLength = 2)]
         public string Nome { get; set; }
         
-        [Required(ErrorMessage = "O Campo {0} é obrigatório")]
-        [StringLength(14, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracters", MinimumLength = 11)]
-        public string Descricao { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
         
         [DisplayName("Tipo")]
         public TipoFornecedor TipoFornecedor { get; set; }
